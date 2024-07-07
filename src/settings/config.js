94be30768a61f490a1eaf1c7703e4033a5cd6db3
Felipe Becker dos Santos <beckerfelipee@gmail.com>
2024-07-07 20:17:31 +0100

    Update config.js

--- src/settings/config.js
+++ src/settings/config.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 module.exports = {
-    token: process.env.TOKEN || "MTIxNjQzODkyNDc5NzQxMTMzOA.GvdNhj.ojy6se0_ijyy3GQtwwvtxJ8eDLVCk4-xXYje3o", // <==== PASTE YOU TOKEN
-    prefix: process.env.PREFIX || "f?", // <==== SET YOU PRERIX BOT [ OWNER COMMANDS ]
-    color: process.env.EMBED_COLOR || "#c50632", // <==== YOU EMBEDED HEX COLOR
-    owner: process.env.OWNER_ID || "434795654527713291", // <==== BOTS OWNER ID
-    guildLogs: process.env.GUILD_LOGS || "761335625436889088", // <==== YOUR SERVER JOIN LEFT LOGS CHANNEL ID
+    token: process.env.TOKEN || "", 
+    prefix: process.env.PREFIX || "f?",
+    color: process.env.EMBED_COLOR || "#c50632",
+    owner: process.env.OWNER_ID || "",
+    guildLogs: process.env.GUILD_LOGS || "", // 
     leaveTimeout: process.env.LEAVE_TIMEOUT || "10000", // <==== SET LEAVE TIMEOUT WHEN BOT WAS ALONE || 1000 = 1sec
     disableYouTube: parseBoolean(process.env.DISABLE_YOUTUBE || "false"), // <==== SET "TRUE OR FALSE" | ENABLE/DISABLE YOUTUBE FEATURES. DISABLING THIS WILL MAKE "AUTOPLAY" COMMANDS USELESS!!!
 
@@ -13,8 +13,8 @@ module.exports = {
     playSource: process.env.PLAY_SOURCE || "ytsearch", // <==== SET YOUR PLAY SOURCE || "ytsearch","ytmsearch","scsearch"
     poruOptions: {
         defaultPlatform: process.env.DEFAULT_SOURCE || "ytsearch", // <==== SET DEFAULT SOURCE || "ytsearch","ytmsearch","scsearch"
-        clientID: process.env.SPOTIFY_ID || "2bf056dad36141e1ad71643e4a5fb576", // <==== SPOTIFY CLIENT ID
-        clientSecret: process.env.SPOTIFY_SECRET || "e84c65966d074af3bf86cc9fcf9f2f3a", // <==== SPOTIFY CLIENT SECRET
+        clientID: process.env.SPOTIFY_ID || "", // 
+        clientSecret: process.env.SPOTIFY_SECRET || "", // 
         reconnectTries: 2, // <==== TOTAL ATTEMPS TO TRY IF RECONNECT FAILED. YOU CAN CHANGE IT TO "Infinity" FOR UNLIMITED ATTEMPS.
         playlistLimit: 2, // <==== 1 = 100 TRACKS
         albumLimit: 1, // <==== 1 = 50 TRACKS
@@ -120,11 +120,11 @@ module.exports = {
         },
     ],
 
-    mongoUri: process.env.MONGO_URI || "mongodb+srv://beckerfelipee:GvvLUGGGlbFHtxl3@clusterkikobeats.0vwp3xp.mongodb.net/?retryWrites=true&w=majority&appName=ClusterKikoBeats", // <==== YOUR MONGODB LINK
-    supportUrl: process.env.SUPPORT_URL || "https://discord.com", // <==== YOUR SUPPORT SERVER LINK
-    voteUrl: process.env.VOTE_URL || "https://discord.com", // <==== YOUR VOTE URL
-    inviteUrl: process.env.INVITE_URL || "https://discord.com/oauth2/authorize?client_id=1216438924797411338&permissions=8&scope=bot+applications.commands", // <==== YOUR BOT INVITE LINK
-    imageUrl: process.env.IMAGE_URL || "https://www.solidbackgrounds.com/images/800x600/800x600-air-force-dark-blue-solid-color-background.jpg", // <==== YOUR IMAGE LINK 
+    mongoUri: process.env.MONGO_URI || "", 
+    supportUrl: process.env.SUPPORT_URL || "", 
+    voteUrl: process.env.VOTE_URL || "",
+    inviteUrl: process.env.INVITE_URL || "", 
+    imageUrl: process.env.IMAGE_URL || "", 
 };
 
 function parseBoolean(value) {
